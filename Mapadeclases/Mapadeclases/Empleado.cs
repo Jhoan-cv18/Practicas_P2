2cf27ce19eb2cadecb22784db43691b9f1ed4efd
Jhoan-cv18 <jhoanmanuelcastillovaldez@gmail.com>
2025-10-11 22:00:45 -0400

    Update Empleado.cs

--- Mapadeclases/Mapadeclases/Empleado.cs
+++ Mapadeclases/Mapadeclases/Empleado.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Mapadeclases
+﻿namespace Mapadeclases
 {
-    internal class Empleado
+    public class Empleado : MiembroDeLaComunidad
     {
+        public string Cargo { get; set; }
+        public decimal Salario { get; set; }
+
+        public void MostrarDatosEmpleado()
+        {
+            Console.WriteLine($"Empleado: {Nombre}, Cargo: {Cargo}, Salario: {Salario}");
+        }
     }
 }
+

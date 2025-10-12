3783bb860e06a75f175188f7e5220deb05e32665
Jhoan-cv18 <jhoanmanuelcastillovaldez@gmail.com>
2025-10-11 22:05:43 -0400

    Create Docente.cs

---
+++ Mapadeclases/Mapadeclases/Docente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mapadeclases
+{
+    public class Docente : Empleado
+    {
+        public string Materia { get; set; }
+
+        public void Enseñar()
+        {
+            Console.WriteLine($"{Nombre} está enseñando {Materia}.");
+        }
+    }
+}

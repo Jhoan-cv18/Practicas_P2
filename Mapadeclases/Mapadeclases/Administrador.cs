6dd50df04b5d612054716250e78b932a2489246f
Jhoan-cv18 <jhoanmanuelcastillovaldez@gmail.com>
2025-10-11 22:13:22 -0400

    clase Administrador que hereda de Docente

---
+++ Mapadeclases/Mapadeclases/Administrador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapadeclases
+{
+    public class Administrador : Docente
+    {
+        public void Gestionar()
+        {
+            Console.WriteLine($"{Nombre} está gestionando recursos en el área de {Materia}.");
+        }
+    }
+}

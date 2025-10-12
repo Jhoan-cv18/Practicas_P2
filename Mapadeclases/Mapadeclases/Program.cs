459cd11d0265ff20dd017526b1612c0bb05e76a0
Jhoan-cv18 <jhoanmanuelcastillovaldez@gmail.com>
2025-10-11 22:29:14 -0400

    Prueba con Administrador y Administrativo

--- Mapadeclases/Mapadeclases/Program.cs
+++ Mapadeclases/Mapadeclases/Program.cs
@@ -19,6 +19,31 @@ namespace Mapadeclases
             maestro.ExplicarTema();
 
             Console.WriteLine("------------------------");
+           
+            // Prueba con Administrador
+            Administrador admin = new Administrador
+            {
+                Nombre = "Laura Gómez",
+                Edad = 35,
+                Materia = "Informática"
+            };
+            admin.MostrarInformacion();
+            admin.Gestionar();
+
+            Console.WriteLine("------------------------");
+
+            // Prueba con Administrativo
+            Administrativo adm = new Administrativo
+            {
+                Nombre = "Pedro Ramírez",
+                Edad = 29,
+                Cargo = "Secretario",
+                Salario = 25000
+            };
+            adm.MostrarInformacion();
+            adm.ProcesarDocumentos();
+
+            Console.WriteLine("------------------------");
 
         }
     }

133880bcba29d2dac12fff603abaa79def5b03b6
Jhoan-cv18 <jhoanmanuelcastillovaldez@gmail.com>
2025-10-11 21:36:31 -0400

    CREANDO CLASE

---
+++ Mapadeclases/Mapadeclases/MiembroDeLaComunidad.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapadeclases
+{
+    public class MiembroDeLaComunidad
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+        public virtual void MostrarInformacion()
+        {
+            Console.WriteLine($"Nombre: {Nombre}, Edad: {Edad}");
+        }
+    }
+        

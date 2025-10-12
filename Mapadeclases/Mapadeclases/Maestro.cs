0b223adbb41cb783b94d509d1d0ff7f916b0ad62
Jhoan-cv18 <jhoanmanuelcastillovaldez@gmail.com>
2025-10-11 22:11:51 -0400

    agregar clase Maestro que hereda de Docente

---
+++ Mapadeclases/Mapadeclases/Maestro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapadeclases
+{
+    public class Maestro : Docente
+    {
+        public void ExplicarTema()
+        {
+            Console.WriteLine($"{Nombre} está explicando el tema de {Materia}.");
+        }
+    }
+}

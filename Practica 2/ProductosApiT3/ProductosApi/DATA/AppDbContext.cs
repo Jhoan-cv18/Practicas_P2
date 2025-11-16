4f9c37cd9611b620a3f69588f627e85875e9284d
Jhoan-cv18 <jhoanmanuelcastillovaldez@gmail.com>
2025-11-16 00:41:03 -0400

    Copiando la API de productos para usarla en la Tarea 3

---
+++ Practica 2/ProductosApiT3/ProductosApi/DATA/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductosApi.Models;
+
+namespace ProductosApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Tabla Products
+        public DbSet<Product> Products { get; set; }
+    }
+}

9e9cf66c70724af173248838d815c2e15520e41f
Jhoan-cv18 <jhoanmanuelcastillovaldez@gmail.com>
2025-11-16 01:14:12 -0400

    Agregué entidad Product al dominio

---
+++ Practica 2/ProductosApiT3/Productos.Domain/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿using Productos.Domain.Core;
+
+namespace Productos.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}

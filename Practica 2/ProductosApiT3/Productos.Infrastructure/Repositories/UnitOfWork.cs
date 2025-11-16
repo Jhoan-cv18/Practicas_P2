9b6bdcd0e6f8a979b6bb66e95d5d6ee9769ec498
Jhoan-cv18 <jhoanmanuelcastillovaldez@gmail.com>
2025-11-16 02:42:31 -0400

    Agregué UnitOfWork para manejar los repositorios

---
+++ Practica 2/ProductosApiT3/Productos.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Productos.Infrastructure.Context;
+
+namespace Productos.Infrastructure.Repositories
+{
+    public class UnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        // Aquí exponemos los repositorios
+        public ProductRepository Products { get; }
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+            Products = new ProductRepository(_context);
+        }
+
+    }
+}

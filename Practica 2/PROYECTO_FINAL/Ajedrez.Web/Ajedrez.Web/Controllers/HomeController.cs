507f051864f3edcb1830843bb15578cec8264855
Jhoan-cv18 <jhoanmanuelcastillovaldez@gmail.com>
2025-12-10 23:41:02 -0400

    CREANCION ARQUITECTURA

---
+++ Practica 2/PROYECTO_FINAL/Ajedrez.Web/Ajedrez.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ajedrez.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
